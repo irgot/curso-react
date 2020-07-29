efc46ed34a5b99ddad8b03a425930ff452318845
igor.ferreira.souza@gmail.com <igor.ferreira.souza@gmail.com>
2020-07-28 21:14:09 -0300

    angular

---
+++ todo-app/frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-svg-core/styles.css' 
+
+import React,{Component} from 'react'
+
+
+export default props => (
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+)
