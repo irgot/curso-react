364f30293992d17aaa4ba61fd54626ed25324313
IGOR SOUZA <igor.ferreira.souza@gmail.com>
2020-07-30 00:04:53 -0300

    Angular v1

--- angAdminLte/src/app/app.module.ts
+++ angAdminLte/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { SidebarComponent } from './components/template/sidebar/sidebar.component';
+import { HomeComponent } from './components/views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
